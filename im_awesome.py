b8a350760d5b88d6192384cf34638b311d735745
AnayaThombare <anayathombare@gmail.com>
2025-12-02 18:49:11 -0800

    Update im_awesome.py

--- im_awesome.py
+++ im_awesome.py
@@ -5,3 +5,16 @@ is_student = True
 print(name)
 print(zipcode)
 print(is_student)
+
+age = int(input("Put your age"))
+
+if age >= 60:
+  print("You are a senior.")
+
+if age >= 18:
+  print("You are a adult.")
+
+if age < 18:
+  print("You are a minor.")
+
+  
